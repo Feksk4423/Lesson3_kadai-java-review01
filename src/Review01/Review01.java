7e7bf0c1f3d15415e4ab45c121d9a8f64a98e8af
keisu <keisu@LAPTOP-8NVPL5MB>
2024-01-17 14:44:02 +0900

    Review01

---
+++ src/Review01/Review01.java
@@ -0,0 +1,17 @@
+package Review01;
+
+public class Review01 {
+
+    public static void main(String[] args) {
+
+int money = 1500;
+int tax = taxmethod(money, (int)(money * 0.10));
+int result = money + tax;
+System.out.println(money + "の商品の税込価格は" + result + "（消費税は" + tax + "円）です。" );
+    }
+
+public static int taxmethod(int money, int result ) {
+    int tax = (int)(money * 0.10);
+    return tax;
+    }
+}
