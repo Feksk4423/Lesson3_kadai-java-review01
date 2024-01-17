6ecb07b2f0c48fa00513da51869c2047d3c7f057
Feksk4423 <155008988+Feksk4423@users.noreply.github.com>
2024-01-17 16:18:05 +0900

    Update Review01.java

--- src/Review01/Review01.java
+++ src/Review01/Review01.java
@@ -5,12 +5,12 @@ public class Review01 {
     public static void main(String[] args) {
 
 int money = 1500;
-int tax = taxmethod(money, (int)(money * 0.10));
+int tax = tax(money, (int)(money * 0.10));
 int result = money + tax;
-System.out.println(money + "の商品の税込価格は" + result + "（消費税は" + tax + "円）です。" );
+System.out.println(money + "円の商品の税込価格は" + result + "（消費税は" + tax + "円）です。" );
     }
 
-public static int taxmethod(int money, int result ) {
+public static int tax(int money, int result ) {
     int tax = (int)(money * 0.10);
     return tax;
     }
